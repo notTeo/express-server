c1c112a840265f88e5bb28322284b467debc660d
Nikos Theodosis <nikostheodosis05@gmail.com>
2021-11-01 20:25:39 +0200

    basic structure, preparation  for  todos

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const server = express()
+
+const key = "12345"
+
+const users = [
+  {
+    user1: {
+        id: {},
+        name: {},
+        lastName: {},
+        email: {},
+        age: {},
+    }
+    // id
+    // name
+    // lastName
+    // email
+    // age
+    // job
+  }
+]
+
+server.use(bodyParser.json())
+
+server.post("/hi", (request, response) => {
+//  if (!request.headers || request.headers["secret-key"] !== key) {
+//    throw new Error("I don't know you")
+//  }
+  console.log({body: request.body})
+  const name = request.body["name"]
+  const lastname = request.body["lastname"]
+  console.log({ name })
+  console.log({ lastname })
+  response.send(`Hi ${name} ${lastname}`)
+})
+
+// TODOS:
+// put this all in a git repo
+// add prettier
+// multiple small commits (with each commit containing a group of changes)
+// GET /users (returns all users)
+// GET /user/:id (returns user with that id or returns undefined)
+// POST /user (reads the body, and adds a new entry in the users array)
+// PUT /user/:id (finds the user with that id, reads the body, and updates the user)
+// DELETE /user/:id (finds the user with that id, and removes it from the users array)
+
+
+server.listen(3000, () => {
+  console.log("Server Started")
+})
